71413b33226b222c9551df63d001bd466e00ae66
Tanmay Shashi Patel <patelta2@ganymede.egr.msu.edu>
2021-02-25 20:46:56 -0500

    Adding Observer.py

--- hw-02/Observer.py
+++ hw-02/Observer.py
@@ -17,21 +17,49 @@ class Observer():
         '''
         self.im1_filename = im1_filename
         self.im2_filename = im2_filename
-        self.load_images(im1_filename,im2_filename)
+        #self.load_images(im1_filename,im2_filename)
         
-    def load_images():
+    def load_images(self):
         '''
         This function is incomplete! It is missing the appropriate input vales
         and the "pass" should be replaced with the appropriate code.
         Update this docstring to explain what the function does (or should do).
+        
+        Open red then infrared file using fits.
+        Save the data for the two images as a dictionary
         '''
-        pass
+        im1_data = fits.getdata(self.im1_filename, ext = 0)
+        im2_data = fits.getdata(self.im2_filename, ext = 0)
+        
+        self.im1_data = im1_data
+        self.im2_data = im2_data
+        
+        
+    
+    
+    def calc_stats(self):
+        '''
+        Print mean and std for both images and print names of 
+        each file.
+        '''
+        
+        print(self.im1_filename)
+        print("Image mean:",round(np.mean(self.im1_data),2))
+        print("Image standard deviation:",round(np.std(self.im1_data),2))
+        
+        print(self.im2_filename)
+        print("Image mean:",round(np.mean(self.im2_data),2))
+        print("Image standard deviation:",round(np.std(self.im2_data),2))
+        
+       
+
     
     def make_composite(self):
         '''
         This function is incomplete! Make sure to finish it and
         then update this docstring to explain what the function does!
         '''
+        
         # Define the array for storing RGB values
         rgb = np.zeros((self.im1_data.shape[0],self.im1_data.shape[1],3))
         
@@ -41,3 +69,41 @@ class Observer():
         # Compute the red channel values and then clip them to ensure nothing is > 1.0
         rgb[:,:,0] = 1.5 * (self.im2_data.astype("float")/norm_factor)
         rgb[:,:,0][rgb[:,:,0] > 1.0] = 1.0
+        
+        # Compute the green channel values and then clip them to ensure nothing is > 1.0
+        rgb[:,:,1] = ((self.im2_data.astype("float") + self.im1_data.astype("float"))/2)/norm_factor
+        rgb[:,:,1][rgb[:,:,1] > 1.0] = 1.0
+    
+        # Compute the blue channel values and then clip them to ensure nothing is > 1.0
+        rgb[:,:,2] = (self.im1_data.astype("float"))/norm_factor
+        rgb[:,:,2][rgb[:,:,2] > 1.0] = 1.0
+        
+        plt.imshow(rgb, origin = "lower")
+ 
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
